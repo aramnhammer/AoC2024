7cc74aefffbbb8c22e67969274ff6bcf6593d891
aram <aramnhammer@outlook.com>
2024-12-30 14:53:12 -0800

    WIP: trying #2 part 2 in ruby

--- 2/main.rb
+++ 2/main.rb
@@ -1,17 +1,29 @@
-def isSafe?(arr)
-  puts " array: #{arr}"
+def validate(arr)
   increasing = true
   decreasing = true
   valid_differences = true
+  num_bad_levels = 0
+  bad_level_idx = nil
   ret = false
+
   (0...arr.length - 1).each do |i|
     diff = arr[i + 1] - arr[i]
     increasing &= diff > 0
     decreasing &= diff < 0
     valid_differences &= diff.abs.between?(1, 3)
     ret = valid_differences && (increasing || decreasing)
-    puts ret
-    puts "diff: #{diff}: increasing:#{increasing}, decreasing: #{decreasing} valid_diff: #{valid_differences}, result: #{ret}"
+    num_bad_levels = !ret ? num_bad_levels + 1 : num_bad_levels
+    bad_level_idx = i
+  end
+  [ret, num_bad_levels, bad_level_idx]
+end
+
+def isSafe?(arr)
+  bad_level_idx, num_bad_levels, ret = validate(ret)
+  if num_bad_levels == 1
+    arr.delete_at(idx)
+    puts arr
+    _,_,ret = validate(arr)
   end
   return ret
 end
